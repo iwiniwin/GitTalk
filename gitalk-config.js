76794e10ec14698ecf44e1dfb7517f1b02dcfc10
LensarZhang <LensarZhang@126.com>
2019-10-17 18:48:03 +0800

    ﻿fix: 修改配置
    详细信息:
    
    # 提交类型字段
    # feat：新功能（feature）
    # fix： 修补bug
    # docs：文档（documentation）
    # refactor：重构（即不是新增功能，也不是修改bug的代码变动）
    # test：增加测试

--- gitalk-config.js
+++ gitalk-config.js
@@ -1,10 +1,11 @@
+document.write("<script src='/js/src/md5.min.js'></script>");
 var gitalk = new Gitalk({
   "clientID": "267fb8efbb1d0244e095",
   "clientSecret": "769dd0cc63a535b08920f1ef342ac51a58c24d83",
   "repo": "cooee.github.io",
   "owner": "cooee",
   "admin": ["cooee"],
-  "id": decodeURI(page.url),      
+  "id": md5(location.pathname),      
   "distractionFreeMode": false  
 });
 gitalk.render("gitalk-container");
