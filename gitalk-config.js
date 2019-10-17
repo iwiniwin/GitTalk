1b72c5aec7e64084e558e7f097a73e99ecf2f522
LensarZhang <LensarZhang@126.com>
2019-10-17 17:53:39 +0800

    ﻿fix: 测试gittalk
    详细信息:
    
    # 提交类型字段
    # feat：新功能（feature）
    # fix： 修补bug
    # docs：文档（documentation）
    # refactor：重构（即不是新增功能，也不是修改bug的代码变动）
    # test：增加测试

--- gitalk-config.js
+++ gitalk-config.js
@@ -1,7 +1,7 @@
 var gitalk = new Gitalk({
   "clientID": "0d890289e8f2218d34d2",
   "clientSecret": "e52587e99df3f189e1daf5dfd1417a131af41792",
-  "repo": "iwiniwin.github.io/GitTalk",
+  "repo": "iwiniwin.github.io",
   "owner": "iwiniwin",
   "admin": ["iwiniwin"],
   "id": location.pathname,      
